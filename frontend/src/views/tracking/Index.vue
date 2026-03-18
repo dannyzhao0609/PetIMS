9f28d48674ba50c3f026d61f85a5f1855be053a8
dannyzhao0609 <dannyzhao0609@users.noreply.github.com>
2026-03-18 11:08:24 +0800

    修复高德地图2.0控件加载问题，在URL中添加plugin参数

--- frontend/src/views/tracking/Index.vue
+++ frontend/src/views/tracking/Index.vue
@@ -191,7 +191,7 @@ const initMap = () => {
   console.log('开始加载AMap脚本...')
   const script = document.createElement('script')
   script.type = 'text/javascript'
-  script.src = 'https://webapi.amap.com/maps?v=2.0&key=c1778fe007bcf29174b1d9a68e8204c9&callback=initAMapCallback'
+  script.src = 'https://webapi.amap.com/maps?v=2.0&key=c1778fe007bcf29174b1d9a68e8204c9&plugin=AMap.ToolBar,AMap.Scale&callback=initAMapCallback'
   
   window.initAMapCallback = () => {
     console.log('AMap脚本加载完成回调')
@@ -217,9 +217,6 @@ const createMap = () => {
       zoom: 4,
       center: [105.0, 35.0]
     })
-    
-    map.addControl(new AMap.ToolBar())
-    map.addControl(new AMap.Scale())
     
     map.on('complete', () => {
       console.log('地图加载完成')
